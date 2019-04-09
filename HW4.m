6b7d95f2001a01fed32126f7dee0850fca89343b
ShadowMole <steven.bruman@yahoo.com>
2019-04-09 12:44:58 -0400

    Matlab mostly works, kind of

--- HW4.m
+++ HW4.m
@@ -1,23 +1,31 @@
 function minMaxes = minMax()
+    imnum = 1;
     for class = 0:3
         for num = 1:5
+            
             filename = strcat(num2str(class),'_',num2str(num));
             image = imread(strcat('Images/', filename,'.png'));
-            image = im2bw(image);
+            subplot(4,10,imnum);
+            imshow(image, []);
+            imnum = imnum + 1;
+            image = ~(im2bw(image));
             img = regionprops(image);
+            subplot(4,10,imnum);
+            imshow(image, []);
+            imnum = imnum + 1;
             % disp("filename: " + filename);
             centroid = img.Centroid;
             % disp(centroid);
-           % disp("img: " + img);
+           %disp("img: " + img);
             x = centroid(1);
             y = centroid(2);
-            structElementSize = 8;
+            structElementSize = 50;
             se = strel('disk', structElementSize);
             smallIM = imerode(image, se);
             peri =  imabsdiff(image, smallIM);
             minMaxes = zeros(32,2);
             for i = 1:32
-                minMaxes(i, 1) = 1000;
+                minMaxes(i, 1) = 2000;
                 minMaxes(i, 2) = 0;
             end
             [rows,cols] = size(peri);
@@ -28,9 +36,9 @@ function minMaxes = minMax()
 %                         disp("x: " + x);
                         % from 0 to 360 degrees
                         angle = atan2d(col-y,row-x) + 180;
-                        disp("angle: " + angle);
+                        %disp("angle: " + angle);
                         index = mod((int64(double(angle)/11.25)),32) +1;
-                        disp("index: " + index);
+                        %disp("index: " + index);
                         dist = sqrt((col-y).*(col-y)+(row-x).*(row-x));
                         if(dist < minMaxes(index,1))
                             minMaxes(index,1) = dist;
@@ -44,7 +52,10 @@ function minMaxes = minMax()
             fid = fopen( strcat('Data/',filename,'.txt'), 'wt' );
             for J = 1 : 32
                 for K = 1 : 2
-                    fprintf( fid, '%f\n', minMaxes(J,K));
+                    if(minMaxes(J,K) == 0 || minMaxes(J,K) == 2000)
+                        disp(strcat(filename,' ', J, ' ', K));
+                    end
+                    fprintf( fid, '%f,', minMaxes(J,K));
                 end
             end
             fclose(fid);
